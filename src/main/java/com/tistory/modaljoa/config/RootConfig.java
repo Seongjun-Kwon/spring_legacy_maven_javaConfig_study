90772becad615b2a0e3ea7584c170054cb942470
Kwon seong jun <tjdwns1865@naver.com>
2022-02-22 06:49:30 +0900

    의존성 주입 test

--- src/main/java/com/tistory/modaljoa/config/RootConfig.java
+++ src/main/java/com/tistory/modaljoa/config/RootConfig.java
@@ -1,7 +1,9 @@
 package com.tistory.modaljoa.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"com.tistory.modaljoa"})
 public class RootConfig {
 }
---
+++ src/test/java/com/tistory/modaljoa/sample/SampleTests.java
@@ -0,0 +1,31 @@
+package com.tistory.modaljoa.sample;
+
+import com.tistory.modaljoa.config.RootConfig;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RootConfig.class})
+@Log4j
+public class SampleTests {
+
+    @Setter(onMethod = @__({@Autowired}))
+    private Restaurant restaurant;
+
+    @Test
+    public void testExist() {
+
+        assertNotNull(restaurant);
+
+        log.info(restaurant);
+        log.info("-----------------------------------");
+        log.info(restaurant.getChef());
+    }
+}
