ae6348a2e2b6034d25c7b95a86f097514f82ce1d
Kwon seong jun <tjdwns1865@naver.com>
2022-03-08 04:04:07 +0900

    DB layer CRUD 구현

--- src/main/java/com/tistory/modaljoa/mapper/BoardMapper.java
+++ src/main/java/com/tistory/modaljoa/mapper/BoardMapper.java
@@ -8,4 +8,14 @@ public interface BoardMapper {
 
 //    @Select("SELECT * FROM tbl_board WHERE bno > 0")
     public List<BoardVO> getList();
+
+    public void insert(BoardVO board);
+
+    public void insertSelectKey(BoardVO board);
+
+    public BoardVO read(Long bno);
+
+    public int delete(Long bno);
+
+    public int update(BoardVO board);
 }
