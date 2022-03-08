cc0ad56a976ca92b5f5cd31d8eb79ab839aff08d
Kwon seong jun <tjdwns1865@naver.com>
2022-03-09 00:12:53 +0900

    화면 처리

--- src/main/java/com/tistory/modaljoa/config/WebConfig.java
+++ src/main/java/com/tistory/modaljoa/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.tistory.modaljoa.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletRegistration;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -24,5 +26,16 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
     protected void customizeRegistration(ServletRegistration.Dynamic registration) {
 
         registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        return new Filter[]{characterEncodingFilter};
     }
 }
